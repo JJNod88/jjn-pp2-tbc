d8e8bb292cb2adee9e0befad46134c03165ab0ea
JJNod88 <j.noddings@hotmail.com>
2021-12-14 13:42:09 +0000

    Added some logic for the win, lose and draw comparisons in a function

--- README.md
+++ README.md
@@ -24,8 +24,11 @@ Responsiveness
 features
 Card game style battle using the Top trumpz method of choosing your cards best ability to overpower your competitors.
 it consists of a header element, game area, score area and a feedback form where you can requests new heroes to be added.
+
+
 credits
 CloudConvert for webp converter imagecompressor.com to reduce image size
+w3schools for switch and case statements - provided a good basis for my main games win / lose capability.
 
 bugs
-bug: when i originally built my first initial function, i was testing my attribute buttons to with alerts to make sure the iteration and code was working. Nothing was happening at all, i tried changing the values of my data-choices etc. I ran the code on JQuery, it identified there was a problem with line 71. Solution: i had all of my projects below my code, i commented them out, tested again and it worked. my alerts were working!
+My three main elements of rock, paper and scissors were in global Lets. when i first began to create my code, i did not really understand where they needed to go. 
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 console.log('hey');
 
-// Needed to obtain the 3 main elements for playing the game. I did this putting them in "let's"
+// Needed to obtain the 3 main elements for playing the game. I did this putting them in "let"
 let rockIcon = document.getElementById('rock');
 let paperIcon = document.getElementById('paper');
 let scissorsIcon = document.getElementById('scissors');
@@ -9,13 +9,51 @@ let scissorsIcon = document.getElementById('scissors');
 /** 
  * Main function. I needed to iterate through the choices in an array that returned a value.
  * I did math floor for a whole integer, and random * 3 (the number of possible choices)
+ * it is labelled computerChoice as this is what i need to use for my main game function as a user opposes it
  */
-function randomChoice {
+function computerChoices() {
     let userChoices = ['rock', 'paper', 'scissors'];
     let randomNumber = Math.floor(Math.random() * 3);
         return userChoices[randomNumber];
 }
 
+/**
+ * I have added event listener to each of the main elements. 
+ * using 'the game('element')' it then passes the value of each to the main game function
+ */
+function mainGameEventLsiteners() {
+    rockIcon.addEventListener('click', function() {
+        mainGame('rock');
+    });
+    paperIcon.addEventListener('click', function() {
+        mainGame('paper');
+    });
+    scissorsIcon.addEventListener('click', function() {
+        mainGame('scissors');
+    });
+}
+
+/** 
+ * This is my attempt at the logic. I used switch and case for the 
+ */
+function mainGame(userChoices) {
+    let computerChoice = computerChoices();
+    // win
+        switch (userChoices + computerChoice) {
+            case    'paperrock':
+            case    'rockscissors':
+            case    'scissorspaper':
+                console.log('Awesome, you won')
+            case    'rockpaper':
+            case    'paperscissors':
+            case    'scissorsrock':
+                console.log('Oh no, you lost. Better look next time!')
+            case    'rockrock':
+            case    'scissorscissor':
+            case    'paperpaper':
+                console.log('Draw! Close one.')
+        }
+}
 
 // function for collating the score when user wins
 function userWin() {
