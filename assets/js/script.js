58ee15ccc7db7b5db31def6d3a52c7534e6be129
JJNod88 <j.noddings@hotmail.com>
2021-12-15 17:29:56 +0000

    Added event listener to the Rules button with innerHTML and content

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,33 +1,63 @@
 console.log('hey');
 
 
-// These lets are my storage variables
-var userScore = 0;
-let userScored = document.getElementById('userScoreDigit');
-var computerScore = 0;
-let compScored = document.getElementById('compScoreDigit'); 
-let scoreBoardDiv = document.querySelector('.score-area'); // Used a query selector to get the first element
-let resultDiv = document.querySelector('.result-section');
+// JS variables
+let buttons = document.getElementsByTagName('buttons') // buttons
+
 let rockIcon = document.getElementById('rock'); // User Clicks
 let paperIcon = document.getElementById('paper'); // User Clicks
 let scissorsIcon = document.getElementById('scissors'); // User Clicks
-let result;
-let buttons = document.getElementsByTagName('buttons') // buttons
+let result; // User click result
+
+var userScore = 0;
+let userScored = document.getElementById('userScoreDigit'); // User score display
+var computerScore = 0;
+let compScored = document.getElementById('compScoreDigit');  // Comp score display 
+
+let scoreBoardDiv = document.querySelector('.score-area'); // Used a query selector to get the first element
+let resultDiv = document.querySelector('.result-section');
 
 
-// Added event listeners for the buttons once the dom has loaded
+
+/** 
+ * Added Event Listeners following the loading of the Dom Content
+**/
 document.addEventListener("DOMContentLoaded", function () {
 
     for (let button of buttons)
         button.addEventListener("click", function () {
             if (this.getAttribute('data-type') === "rules-btn") {
-
+                let rulesDiv = document.getElementById('gameRules');
+                rulesDiv = `
+                <h2>Here is how to play our simple game:</h2>
+                <ol>
+                    <li>Below are three choices of Rock, Paper and Scissors</li>
+                    <li>Rock Beats Scissors. Scissors beats Paper. Paper beats Rock</li>
+                    <li>Your aim is to pick the icon that beats the computers chosen icon</li>
+                    <li>Each winner receives a point. The first to 5 points wins!</li>
+                </ol>
+                <p>Good luck, and enjoy!</p>
+                `;
+            }
+        }
+    };
+/** 
+ * I needed to iterate through the choices in an array that returned some form of value.
+ * Mathfloor is used to capture a whole integer, and random * 3 (the number of possible choices)
+ * it is labelled computerChoice as this is what i need to use for my main game function as a user opposes it
+ */
+ function setChoices() {
+    let choiceArray = ['rock', 'paper', 'scissors'];
+        let randomNumber = Math.floor(Math.random() * 3); 
+            return choiceArray[randomNumber];
+}
+console.log(setChoices())
 
 /**
  * I have added event listener to each of the main elements. 
  * using 'thegame('element')' it then passes the value of each to thegame function
  */
- function mainEListeners() {
+ function userSelectors() {
     rockIcon.addEventListener('click', function() {
         theGame('rock');
     });
@@ -37,12 +67,18 @@ document.addEventListener("DOMContentLoaded", function () {
     scissorsIcon.addEventListener('click', function() {
         theGame('scissors');
     });
+};
+userSelectors();
+
+
+function checkAnswer() {
+
 }
 
-mainEListeners();
-
-
-// linking my clicks with each of the 3 icons which competes with the computers random choice
+/**
+ * 
+ * This is the logic behind my game. Rock beats scissors etc.
+ */
 function theGame(userClicks) {
     let computerChooses = setChoices();
         if (userClicks === computerChooses) {
@@ -74,14 +110,13 @@ function theGame(userClicks) {
 theGame()
 
 
-/** 
- * I needed to iterate through the choices in an array that returned some form of value.
- * Mathfloor is used to capture a whole integer, and random * 3 (the number of possible choices)
- * it is labelled computerChoice as this is what i need to use for my main game function as a user opposes it
- */
- function setChoices() {
-    let choiceArray = ['rock', 'paper', 'scissors'];
-        let randomNumber = Math.floor(Math.random() * 3); 
-            return choiceArray[randomNumber];
+
+
+function addUserScore() {
+
 }
-console.log(setChoices())
+
+
+function addCompScore() {
+
+}
